f2e4f6cc7b3ab5d222ee05658bc05f1f22834a8b
dodo240703 <a20202396@pucp.edu.pe>
2024-11-08 01:56:26 -0500

    configuraciones iniciales

---
+++ GTICS-LAB9-20202396/src/main/java/com/example/gticslab920202396/GticsLab920202396Application.java
@@ -0,0 +1,13 @@
+package com.example.gticslab920202396;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GticsLab920202396Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GticsLab920202396Application.class, args);
+	}
+
+}
---
+++ GTICS-LAB9-20202396/src/test/java/com/example/gticslab920202396/GticsLab920202396ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gticslab920202396;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GticsLab920202396ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
